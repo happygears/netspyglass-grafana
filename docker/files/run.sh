d2002c1bf77572be6a758dcff0649ca2150b0cda
Vadim Kurland <vadim@happygears.net>
2018-04-03 21:51:55 -0700

    working on the new docker image

--- docker/files/run.sh
+++ docker/files/run.sh
@@ -2,7 +2,7 @@
 #
 # Grafana startup script
 
-HAPPYGEARS_DIR="/happygears/grafana"
+HAPPYGEARS_DIR="/happygears"
 
 GRAFANA_HOME_DIR="/opt/grafana"
 GRAFANA_DB="$GRAFANA_HOME_DIR/db/grafana.db"
@@ -11,65 +11,81 @@ GRAFANA_PLUGINS_DIR="$GRAFANA_HOME_DIR/plugins/"
 GRAFANA_PLUGIN_TAR="netspyglass-datasource-v2.tar"
 GRAFANA_API_TOKEN="eyJrIjoidGhXcFVHYnZMNEZtYWo5UDVwVlFEWTdOWFJpV2lmcUwiLCJuIjoiZG9ja2VyX3J1biIsImlkIjoxfQ=="
 
-GRAFANA_DASHBOARDS_DIR="$HAPPYGEARS_DIR/dashboards"
-GRAFANA_DATASOURCES_DIR="$HAPPYGEARS_DIR/data_sources"
+GRAFANA_DASHBOARDS_DIR="$HAPPYGEARS_DIR/grafana/dashboards"
+GRAFANA_DATASOURCES_DIR="$HAPPYGEARS_DIR/grafana/data_sources"
 
-# update NetSpyGlass datasource plugin by copying it from the "storage" directory in the image
-# This ensures that new version of the plugin the image was built with is actually used
-#mkdir -p ${GRAFANA_PLUGINS_DIR} && \
-#    tar -xf "$HAPPYGEARS_DIR/plugins/$GRAFANA_PLUGIN_TAR" --directory ${GRAFANA_PLUGINS_DIR}
+cd ${HAPPYGEARS_DIR}
 
-test -f ${GRAFANA_DB} || {
-    echo "Starting Grafana to create database ..."
-    service grafana-server start
-    while test -f ${GRAFANA_DB_JOURNAL}
-    do
-        ls -la ${GRAFANA_DB_JOURNAL}
-        sleep 1
-    done
-    sleep 5
-    service grafana-server stop
-}
+ls -la
 
-# do this while Grafana is stopped because database is locked when Grafana is running
-n=$(sqlite3 ${GRAFANA_DB} "SELECT name FROM data_source WHERE name='NetSpyGlass-v2'")
-test -z "$n" && {
-    echo "Configuring default user and api key ..."
-    echo "update user set password = 'cf7e09992bb25e0186db66514b5925ffd3851e479156ce55c7a7195ae016f28aab7444002321d7a29be13dee5f612cd9c472', salt = 'F3FAxVm33R' where login = 'admin_c842df8da7ae';" | \
-         sqlite3 ${GRAFANA_DB} || echo "Failed to add data sources"
-    echo "INSERT INTO 'api_key' VALUES(1,1,'docker_run','2dcfb641b7ade3479bba29c2f483c2cf3e70804f1d2afc6cc1daa093972924670652c1ba0a9a4705e47f3db1c8c82cbe540c','Admin','2017-10-27 04:59:34','2017-10-27 04:59:34');" | \
-         sqlite3 ${GRAFANA_DB} || echo "Failed to add data sources"
+PLUGIN_ID=$(jq -r '.["id"]' dist/plugin.json) && \
+    mkdir -p ${GRAFANA_PLUGINS_DIR}/${PLUGIN_ID} && \
+    cp -r dist/* ${GRAFANA_PLUGINS_DIR}/${PLUGIN_ID}/
 
-    echo "Starting Grafana again ..."
-    service grafana-server start
-    sleep 10
+echo "SQL_HOST=$SQL_HOST"
+echo "SQL_USER=$SQL_USER"
 
-    for data_source_file in $(ls ${GRAFANA_DATASOURCES_DIR}/*.json)
-    do
-        echo -n "Adding $data_source_file ..."
-        curl -s --data @${data_source_file} \
-            -H "Content-Type: application/json" \
-            -H "Authorization: Bearer $GRAFANA_API_TOKEN" \
-            http://localhost:3000/api/datasources
-        echo
-    done
-
-}
-
-echo "Grafana environment variables:"
-env | grep GF_
+#
+#
+#
+## update NetSpyGlass datasource plugin by copying it from the "storage" directory in the image
+## This ensures that new version of the plugin the image was built with is actually used
+##mkdir -p ${GRAFANA_PLUGINS_DIR} && \
+##    tar -xf "$HAPPYGEARS_DIR/plugins/$GRAFANA_PLUGIN_TAR" --directory ${GRAFANA_PLUGINS_DIR}
+#
+#test -f ${GRAFANA_DB} || {
+#    echo "Starting Grafana to create database ..."
+#    service grafana-server start
+#    while test -f ${GRAFANA_DB_JOURNAL}
+#    do
+#        ls -la ${GRAFANA_DB_JOURNAL}
+#        sleep 1
+#    done
+#    sleep 5
+#    service grafana-server stop
+#}
+#
+## do this while Grafana is stopped because database is locked when Grafana is running
+#n=$(sqlite3 ${GRAFANA_DB} "SELECT name FROM data_source WHERE name='NetSpyGlass-v2'")
+#test -z "$n" && {
+#    echo "Configuring default user and api key ..."
+#    echo "update user set password = 'cf7e09992bb25e0186db66514b5925ffd3851e479156ce55c7a7195ae016f28aab7444002321d7a29be13dee5f612cd9c472', salt = 'F3FAxVm33R' where login = 'admin_c842df8da7ae';" | \
+#         sqlite3 ${GRAFANA_DB} || echo "Failed to add data sources"
+#    echo "INSERT INTO 'api_key' VALUES(1,1,'docker_run','2dcfb641b7ade3479bba29c2f483c2cf3e70804f1d2afc6cc1daa093972924670652c1ba0a9a4705e47f3db1c8c82cbe540c','Admin','2017-10-27 04:59:34','2017-10-27 04:59:34');" | \
+#         sqlite3 ${GRAFANA_DB} || echo "Failed to add data sources"
+#
+#    echo "Starting Grafana again ..."
+#    service grafana-server start
+#    sleep 10
+#
+#    for data_source_file in $(ls ${GRAFANA_DATASOURCES_DIR}/*.json)
+#    do
+#        echo -n "Adding $data_source_file ..."
+#        curl -s --data @${data_source_file} \
+#            -H "Content-Type: application/json" \
+#            -H "Authorization: Bearer $GRAFANA_API_TOKEN" \
+#            http://localhost:3000/api/datasources
+#        echo
+#    done
+#
+#}
+#
+#echo "Grafana environment variables:"
+#env | grep GF_
+#
+#service grafana-server start
+#sleep 10
+#
+#for dashboard_file in $(ls ${GRAFANA_DASHBOARDS_DIR}/*.json)
+#do
+#    echo -n "Adding or updating $dashboard_file ..."
+#    curl -s --data @${dashboard_file} \
+#        -H "Content-Type: application/json" \
+#        -H "Authorization: Bearer $GRAFANA_API_TOKEN" \
+#        http://localhost:3000/api/dashboards/db
+#    echo
+#done
 
 service grafana-server start
-sleep 10
-
-for dashboard_file in $(ls ${GRAFANA_DASHBOARDS_DIR}/*.json)
-do
-    echo -n "Adding or updating $dashboard_file ..."
-    curl -s --data @${dashboard_file} \
-        -H "Content-Type: application/json" \
-        -H "Authorization: Bearer $GRAFANA_API_TOKEN" \
-        http://localhost:3000/api/dashboards/db
-    echo
-done
 
 tail -F ${GRAFANA_HOME_DIR}/logs/grafana.log

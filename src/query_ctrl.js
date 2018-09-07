c75514c610c9156b567986a936ab4ae8c63cdb21
zabilskaya <mzabilskaya@codeminders.com>
2018-09-07 08:59:07 +0300

    #NET-2393 query used to get the list of possible word for a tag selected in WHERE should not match other conditions

--- src/query_ctrl.js
+++ src/query_ctrl.js
@@ -412,7 +412,7 @@ export class NetSpyGlassQueryCtrl extends QueryCtrl {
                     promise = this.datasource.getSuggestions({
                         type: segments[index - 2].value,
                         variable: this.store.variable,
-                        tags: this._filterPreviousWhereTags(index),
+                        // tags: this._filterPreviousWhereTags(index),
                         scopedVars: this.panel.scopedVars
                     });
                     break;

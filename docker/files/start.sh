4ec8be65ea1e0287459cfd81bc4c449e10643284
Vadim Kurland <vadim@happygears.net>
2018-10-10 09:48:04 -0700

    temporarily turn off volume inititalization

--- docker/files/start.sh
+++ docker/files/start.sh
@@ -21,10 +21,10 @@ PLUGIN_ID=$(jq -r '.["id"]' dist/plugin.json) && \
 
 mkdir -p ${GRAFANA_PROVISIONING_DIR}
 ls -laR ${GRAFANA_PROVISIONING_DIR}
-chown -R grafana.grafana ${GRAFANA_PROVISIONING_DIR}
 
-cp -r ${HAPPYGEARS_DIR}/grafana/provisioning/*  ${GRAFANA_PROVISIONING_DIR}/
-
-ls -laR ${GRAFANA_PROVISIONING_DIR}/
+#chown -R grafana.grafana ${GRAFANA_PROVISIONING_DIR}
+#cp -r ${HAPPYGEARS_DIR}/grafana/provisioning/*  ${GRAFANA_PROVISIONING_DIR}/
+#
+#ls -laR ${GRAFANA_PROVISIONING_DIR}/
 
 /run.sh

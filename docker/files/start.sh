5212f8a39af894cc28b5f82f30f315c8d4b0322f
Vadim Kurland <vadim@happygears.net>
2021-01-11 14:57:22 -0800

    fix COPY

--- docker/files/start.sh
+++ docker/files/start.sh
@@ -19,22 +19,11 @@ id -a
 mkdir -p ${GRAFANA_PLUGINS_DIR}
 mkdir -p ${GRAFANA_DATASOURCES_DIR}
 
-# copy plugins that come with the image to Grafana plugins directory to install and upgrade them
-
 echo "### Generate Grafana datasources.yaml from template ..."
 perl -pe 's{@(\w+)@}{$ENV{$1} // $&}ge' < ${SRC_PLUGINS_DIR}/grafana-datasources.yaml > ${GRAFANA_DATASOURCES_DIR}/datasources.yaml
 
 cp -r ${SRC_PLUGINS_DIR}/* ${GRAFANA_PLUGINS_DIR}
 chown -R grafana ${GRAFANA_PLUGINS_DIR}      || true   # if it is read-only because it is mounted as docker config
-
-#ls -lad ${GRAFANA_PLUGINS_DIR}
-#
-#mkdir -p ${GRAFANA_PROVISIONING_DIR}
-#
-#ls -laR ${GRAFANA_PROVISIONING_DIR}
-#
-#chown -R grafana.grafana ${GRAFANA_PROVISIONING_DIR} || true   # if it is read-only because it is mounted as docker config
-#cp -r ${HAPPYGEARS_DIR}/grafana/provisioning/*  ${GRAFANA_PROVISIONING_DIR}/ || true
 
 ls -laR ${GRAFANA_PROVISIONING_DIR}/
 
